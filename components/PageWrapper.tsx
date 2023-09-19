58712122046812278d37a485b46ab5a55d25d4f8
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2023-09-19 14:43:41 -0400

    add way to hide header+footer thru URL param

--- components/PageWrapper.tsx
+++ components/PageWrapper.tsx
@@ -2,12 +2,23 @@ import * as React from 'react';
 import PortalNavbar from './PortalNavbar';
 import Footer from './Footer';
 
+const showContentOnly = () => {
+    if (typeof window === 'undefined') {
+        return false;
+    } else {
+        const urlParams = new URLSearchParams(window.location.search);
+        return (
+            urlParams.has('contentOnly')
+        );
+    }
+};
+
 const PageWrapper = (props: any) => {
     return (
         <div id={'pageWrapper'}>
-            <PortalNavbar />
+            {showContentOnly() || <PortalNavbar />}
             {props.children}
-            <Footer />
+            {showContentOnly() || <Footer />}
         </div>
     );
 };

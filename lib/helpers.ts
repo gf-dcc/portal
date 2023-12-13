04dd9c2dda3152a2f3c601da5de15d8b297fa8bf
Christina Conrad <114612268+cconrad8@users.noreply.github.com>
2023-12-13 12:52:34 -0500

    Update lib/helpers.ts
    Co-authored-by: Onur Sumer <s.onur.sumer@gmail.com>

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -24,7 +24,7 @@ if (typeof window !== 'undefined') {
     win = {} as any;
 }
 
-export type synapseId = string;
+export type SynapseId = string;
 export type BiospecimenID = string;
 export type ParticipantID = string;
 
